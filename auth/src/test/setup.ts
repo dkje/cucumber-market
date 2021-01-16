e52c561e84c4b403d8d069a71cb9acd9f87390e8
dkje <developer.kje@gmail.com>
2021-01-16 15:33:49 +0900

    signin global helper method added

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -24,3 +33,19 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+//This function can only be used in a test environment
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
