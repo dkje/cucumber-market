abbb0200c0f93fe9e5f22156d3cbf8baa160015e
dkje <developer.kje@gmail.com>
2021-01-18 00:37:22 +0900

    fix middleware and error dependencies

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { User } from "../models/users";
-import { validationRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validationRequest, BadRequestError } from "@cucumber-market/common";
 import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
-import { BadRequestError } from "../errors/bad-request-error";
-import { validationRequest } from "../middlewares/validate-request";
+import { BadRequestError, validationRequest } from "@cucumber-market/common";
 import { User } from "../models/users";
 
 const router = express.Router();
